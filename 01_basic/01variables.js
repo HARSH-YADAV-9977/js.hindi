22b52167e50d61a13335ba6c3cb9c995901bde8c
HARSH-YADAV-9977 <hryni9977@gmail.com>
2026-03-16 20:32:44 +0000

    added variable js file

---
+++ 01_basic/01variables.js
@@ -0,0 +1,12 @@
+const accountId= 144553 // always remain constant
+let acc="harsh@566" // both let and const remain same 
+var pp="dfafhb*0iuy"
+u= "harsh123" // number with half string remain should be under the " " that is string
+
+/* var ko use nahi karte hai becuse issue 
+ in block  scope and function scope 
+*/
+let harsh
+console.table([accountId,acc,pp,harsh])
+// let ek acha tarika hai isko use karna chiyee
+// undefined ta hai gar don't give value to let 
