89fbaf9ce68c792eacee1c6659b2d92a5134012e
HARSH-YADAV-9977 <hryni9977@gmail.com>
2026-03-23 17:55:10 +0000

    datatypes

---
+++ 01_basic/02_variables.js
@@ -0,0 +1,26 @@
+"use strict";//treat all js code as newer version but now a days  if don't write automatically consider newer
+//js ka engin hai browser
+
+// alert(3+3)  //we are not getting alert because using node.js not browser
+  
+
+let name="harsh"  //string type
+let age=20 //numerical type 
+let islog=false// bool 
+
+//automatically string numerical,booletc ho jata hai specific nahi marna padta 
+
+//number  =>(why this is used)  range +-(2 to the power 23-1)
+//bigint  for lager value not used by us 
+//string =>""
+//boolean => true/false
+//null => standlone value (type is object)
+//undefined => varible is intialized but not given value
+// symbol => unique
+
+
+// object
+
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof age)
